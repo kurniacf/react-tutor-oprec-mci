8828b207aeb0cbe806b9fb5a364b13c496627048
Kurnia Cahya Febryanto <kcf170202@gmail.com>
2023-02-18 23:16:22 +0700

    feat: tahap-18 handling fetch error

--- src/Home.js
+++ src/Home.js
@@ -4,23 +4,34 @@ import BlogList from './BlogList';
 function App() {
     const [blogs, setBlogs] = useState([]);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
-            .then(response => response.json())
+            .then(response => {
+                if (!response.ok) { 
+                    throw Error('Error fetching data');
+                } 
+                return response.json();
+            })
             .then(data => {
                 setIsPending(false);
                 setBlogs(data.blogs);
+                setError(null);
             })
-            .catch(error => console.log(error));
+            .catch(error => {
+                setIsPending(false);
+                setError(error.message);
+            });
         }, 1000);
     }, []);
 
     return (
         <div className="App">
+            { error && <div>{ error }</div> }
             { isPending && <div>Loading...</div> }
-            <BlogList blogs={blogs} />
+            { blogs && <BlogList blogs={blogs} /> }
         </div>
     );
 }
