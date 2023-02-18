f581cd91b50169d4d19e70b52815db320fb6b0b7
Kurnia Cahya Febryanto <kcf170202@gmail.com>
2023-02-18 07:31:38 +0700

    feat: tahap-4 multiple components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'Selamat Datang di Blog Saya';
-  const likes = 100;
-
-  const link = 'http://www.google.com';
-
   return (
     <div className="App">
-      <div className='content'>
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-
-        <p>{10}</p>
-        <p>{"Hello, Kurnia"}</p>
-        <p>{[1, 2, 3, 4, 5]}</p>
-        <p>{Math.random() * 10}</p>
-
-        <a href={link}>Google Site</a>
+      <Navbar />
+      <div className="content">
+        <Home />
       </div>
     </div>
   );
