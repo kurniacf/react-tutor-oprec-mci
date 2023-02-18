e65d5a87065a308cfe579ac495e2d176380c0c21
Kurnia Cahya Febryanto <kcf170202@gmail.com>
2023-02-18 16:19:48 +0700

    feat: tahap-11 reusing components

--- src/Home.js
+++ src/Home.js
@@ -4,13 +4,15 @@ import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState([
         {title: 'Welcome to the Black Parade', body: 'I Love You in my dream', author: 'My Chemical Romance', id: 1},
-        {title: 'Norwegian Wood', body: 'We cant be together', author: 'Haruki Murakami', id: 2},
-        {title: '(MCI Love) Miracle Competition In Love', body: 'I like you, but you dont like me', author: 'Kurnia Cahya', id: 3}
+        {title: 'The Recipe for Happy', body: 'I am Happy with You', author: 'Kurnia Cahya', id: 2},
+        {title: 'Norwegian Wood', body: 'We cant be together', author: 'Haruki Murakami', id: 3},
+        {title: '(MCI Love) Miracle Competition In Love', body: 'I like you, but you dont like me', author: 'Kurnia Cahya', id: 4}
     ])
 
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs" />
+            <BlogList blogs={blogs.filter(blog => blog.author === 'Kurnia Cahya')} title="Kurnia's Blogs" />
         </div>
     );
 }
