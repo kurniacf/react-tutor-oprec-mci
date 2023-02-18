6c82467c38720d4d16b2a998604adc81f4abe29b
Kurnia Cahya Febryanto <kcf170202@gmail.com>
2023-02-18 09:05:32 +0700

    feat: tahap-9 outputting lists

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,20 @@
 import {useState} from 'react';
 
 const Home = () => {
-    const [name, setName] = useState('Kurnia');
-    const [age, setAge] = useState(21);
-
-    const handleClick = (e) => {
-        setName('Cahya');
-        setAge(30);
-    }
+    const [blogs, setBlogs] = useState([
+        {title: 'Welcome to the Black Parade', body: 'I Love You in my dream', author: 'My Chemical Romance', id: 1},
+        {title: 'Norwegian Wood', body: 'We cant be together', author: 'Haruki Murakami', id: 2},
+        {title: '(MCI Love) Miracle Competition In Love', body: 'I like you, but you dont like me', author: 'Kurnia Cahya', id: 3}
+    ])
 
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{ name } is { age } years old</p>
-            <button onClick={handleClick}>Click me</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
         </div>
     );
 }
