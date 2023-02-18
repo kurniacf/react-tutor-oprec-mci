c7063fbc114797b6a048281cb4f1985d7a57e9e8
Kurnia Cahya Febryanto <kcf170202@gmail.com>
2023-02-18 19:36:33 +0700

    feat: tahap-16 using json fixed

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,12 +1,13 @@
 const BlogList = ({ blogs }) => {
     return (
         <div className="blog-list">
-            {blogs.map(blog => (
-                <div className="blog-preview" key={blog.id} >
-                    <h2>{ blog.title }</h2>
-                    <p>Written by { blog.author }</p>
-                </div>
-            ))}
+        {blogs.map(blog => (
+            <div className="blog-preview" key={blog.id}>
+                <h2>{blog.title}</h2>
+                <p>Written by {blog.author}</p>
+                <p>{blog.body}</p>
+            </div>
+        ))}
         </div>
     );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,21 @@
-import {useState, useEffect} from 'react';
-import BlogList from "./BlogList";
+import { useState, useEffect } from 'react';
+import BlogList from './BlogList';
 
-const Home = () => {
-    const [blogs, setBlogs] = useState(null)
+function App() {
+    const [blogs, setBlogs] = useState([]);
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
-        .then(res => {
-            return res.json();
-        })
-        .then(data => {
-            setBlogs(data);
-        })
-    }, [])
+        .then(response => response.json())
+        .then(data => setBlogs(data.blogs))
+        .catch(error => console.log(error));
+    }, []);
 
     return (
-        <div className="home">
-            {blogs && <BlogList blogs={blogs} />}
+        <div className="App">
+        <BlogList blogs={blogs} />
         </div>
     );
 }
 
-export default Home;
+export default App;
