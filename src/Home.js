6bb4aef3c94fcf60ca77c3ef877181660ecf6f49
Kurnia Cahya Febryanto <kcf170202@gmail.com>
2023-02-18 18:28:22 +0700

    feat: tahap-16 fetching data

--- src/Home.js
+++ src/Home.js
@@ -2,29 +2,21 @@ import {useState, useEffect} from 'react';
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {title: 'Welcome to the Black Parade', body: 'I Love You in my dream', author: 'My Chemical Romance', id: 1},
-        {title: 'The Recipe for Happy', body: 'I am Happy with You', author: 'Kurnia Cahya', id: 2},
-        {title: 'Norwegian Wood', body: 'We cant be together', author: 'Haruki Murakami', id: 3},
-        {title: '(MCI Love) Miracle Competition In Love', body: 'I like you, but you dont like me', author: 'Kurnia Cahya', id: 4}
-    ])
-
-    const [name, setName] = useState('Kurnia Cahya');
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    const [blogs, setBlogs] = useState(null)
 
     useEffect(() => {
-        console.log('use effect running');
-        console.log(blogs);
-    }, [blogs, name])
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setBlogs(data);
+        })
+    }, [])
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-            <button onClick={() => setName('Labib')}>change name</button>
+            {blogs && <BlogList blogs={blogs} />}
         </div>
     );
 }
