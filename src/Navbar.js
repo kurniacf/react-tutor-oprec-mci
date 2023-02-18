4e1b0dd407459b322e8532c27f23d1d0f54e3ca1
Kurnia Cahya Febryanto <kcf170202@gmail.com>
2023-02-19 01:03:16 +0700

    feat: tahap-22 router links

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>MCI Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create" style={{
+                <Link to="/">Home</Link>
+                <Link to="/create" style={{
                     color: "white",
                     backgroundColor: "#088F8F",
                     borderRadius: "8px"
-                }}>New Blog</a>
+                }}>New Blog</Link>
             </div>
         </nav>
     );
