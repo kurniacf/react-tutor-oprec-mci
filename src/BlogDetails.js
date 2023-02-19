b9937a7d440006cac4ffc64b63272347a4463603
Kurnia Cahya Febryanto <kcf170202@gmail.com>
2023-02-19 10:09:58 +0700

    feat: tahap-25 reusing custom hooks

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,21 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams();
-
+    const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blog/' + id);
+    console.log(blogs);
     return (
         <div className="blog-details">
-            <h2>Blog details - { id }</h2>
+            { isPending && <div>Loading...</div> }
+            { error && <div>{ error }</div> }
+            { blogs && (
+                <article>
+                    <h2>{ blogs.title }</h2>
+                    <p>Written by { blogs.author }</p>
+                    <div>{ blogs.body }</div>
+                </article>
+            )}
         </div>
     );
 }
